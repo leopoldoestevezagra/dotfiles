df54cb5d138d629a6e021ac13434e7a66daead86
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-02-13 20:28:04 +0100

    Small change

--- scripts/WindowManagement/resizeHalfTCB.sh
+++ scripts/WindowManagement/resizeHalfTCB.sh
@@ -3,4 +3,5 @@
 wmctrl -r "Terminal" -e 1,10,40,950,960
 wmctrl -r "Studio Code" -e 1,10,40,945,945
 wmctrl -r "Chromium" -e 1,960,40,950,945
+wmctrl -r "Android Studio" -e 1,960,40,950,945
 wmctrl -r ".pdf" -e 1,930,15,1015,1040
