745b6c5176ea62389e43afa1f8c5d9249fbb00ff
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-02-21 19:09:29 +0100

    Added : New shortcuts

--- scripts/WindowManagement/resizeHalfTCB.sh
+++ scripts/WindowManagement/resizeHalfTCB.sh
@@ -5,3 +5,4 @@ wmctrl -r "Studio Code" -e 1,10,40,945,945
 wmctrl -r "Chromium" -e 1,960,40,950,945
 wmctrl -r "Android Studio" -e 1,960,40,950,945
 wmctrl -r ".pdf" -e 1,930,15,1015,1040
+wmctrl -r "CLion" -e 1,960,40,945,945
