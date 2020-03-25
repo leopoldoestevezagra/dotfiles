78d38860ec42a231a264957da0c884d5a54e96dc
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-03-25 19:03:05 +0100

    Removed : Unused tmux window on work

--- scripts/tmuxWork.sh
+++ scripts/tmuxWork.sh
@@ -10,9 +10,6 @@ tmux send-keys "vim " C-m
 
 tmux new-window -t $session:2 -n bash
 
-tmux new-window -t $session:4 -n vifm
-tmux send-keys "vifm . ~/" C-m
-
 tmux select-window -t $session:1
 
 tmux attach-session -t $session
