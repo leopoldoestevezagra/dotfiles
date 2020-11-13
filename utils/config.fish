96bf162d4edcd988b2edaa0fa5fa67ec8c74130e
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2020-11-13 19:17:09 +0100

    Added : Fish config

---
+++ utils/config.fish
@@ -0,0 +1,22 @@
+set fish_greeting ""
+
+alias q="exit"
+alias pro="cd ~/Documents/Projects"
+alias clases="~/Documents/Clases"
+
+alias ginit="git init"
+alias gstatus="git status"
+alias glogg="git log --oneline --graph"
+alias glog="git log"
+alias gadd="git add ."
+alias gcomm="git commit"
+alias gpush="git push"
+alias gstash="git stash"
+
+alias v="vim"
+alias vf="vifm"
+alias pdfo="evince "
+
+alias vbash="vim ~/.bashrc"
+alias vvim="vim ~/.vimrc"
+alias vtux="vim ~/.tmux.conf"
