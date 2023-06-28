6d0f9903a00ed10e2dfb641a991e9b598200624e
Leopoldo Estevez <l.estevez@inovalabs.es>
2023-06-28 10:13:07 +0200

    Added: Eslint to default configuration

--- nvim/after/plugin/lspconfig.rs.lua
+++ nvim/after/plugin/lspconfig.rs.lua
@@ -12,3 +12,12 @@ lspconfig.tsserver.setup ({
     capabilities = cmp_capabilities,
     on_attach = on_attach
 })
+
+lspconfig.eslint.setup({
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
--- nvim/after/plugin/mason.rs.lua
+++ nvim/after/plugin/mason.rs.lua
@@ -1,5 +1,5 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "tsserver" },
+    ensure_installed = { "lua_ls", "tsserver", "eslint" },
 }
